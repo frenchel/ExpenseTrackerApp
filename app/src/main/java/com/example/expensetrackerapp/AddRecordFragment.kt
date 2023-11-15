1582b303a2805fc43d398b5efa9bfa9e0a642251
Frenchel Laroza <frenchellaroza@gmail.com>
2023-11-16 00:39:16 +0800

    added AddRecordFragment (floating action bar) wala pa yung cancel button

---
+++ app/src/main/java/com/example/expensetrackerapp/AddRecordFragment.kt
@@ -0,0 +1,46 @@
+package com.example.expensetrackerapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class AddRecordFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_add_record, container, false)
+
+        val saveAddRecordButton = view.findViewById<Button>(R.id.saveAddRecord)
+        saveAddRecordButton.setOnClickListener {
+
+            (activity as? MainActivity)?.bottomNavigationView?.selectedItemId = R.id.overview
+
+            val newFragment = OverviewFragment()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.frame_layout, newFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+
+        }
+
+        return view
+    }
+
+
+}
--- app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/expensetrackerapp/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.expensetrackerapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
-    /*HELLOOOOOO -dyogi*/
-    /*push*/
+
+    lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,15 +41,27 @@ class MainActivity : AppCompatActivity() {
         }
         replaceFragment(OverviewFragment())
 
-        /*FLOATING ACTION BUTTON - ADD RECORD*/
 
+        /*FLOATING ACTION BUTTON - ADD RECORD*/
+        var addRecordButton = findViewById<FloatingActionButton>(R.id.fab)
+        addRecordButton.setOnClickListener {
+            showAddRecordFragment()
+            bottomNavigationView.selectedItemId = 0
+        }
     }
 
-    /*FUNCTION FOR REPLACING THE FRAGMENTS*/
+    /*FUNCTION FOR REPLACING THE FRAGMENTS - BOTTOM NAVIGATION*/
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.frame_layout, fragment).commit()
     }
 
-    /*FUNCTION FOR DISPLAYING ADD RECORD DIALOG*/
+    /*FUNCTION FOR ADD RECORD FRAGMENT*/
+    private fun showAddRecordFragment() {
+        val nextFragment = AddRecordFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, nextFragment).commit()
+    }
 
+    private fun FloatingActionButton.setOnClickListener(showAddRecordFragment: Any ) {
+
+    }
 }
